2ee42f479dc7ba5304738932925cca0f1626d2f0
Zaki Raihan <zaki.raihan98@gmail.com>
2018-09-25 14:34:24 +0700

    second commit

---
+++ src/main/java/com/apap/tutorial3/service/CarInMemoryService.java
@@ -0,0 +1,43 @@
+package com.apap.tutorial3.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.apap.tutorial3.model.CarModel;
+
+/**
+ * CarInMemoryService
+ * @author Zaki Raihan
+ *
+ */
+@Service
+public class CarInMemoryService implements CarService {
+	private List<CarModel> archiveCar;
+	
+	public CarInMemoryService() {
+		archiveCar = new ArrayList<>();
+	}
+	
+	@Override
+	public void addCar(CarModel car) {
+		archiveCar.add(car);
+	}
+	
+	@Override
+	public List<CarModel> getCarList() {
+		return archiveCar;
+	}
+	
+	@Override
+	public CarModel getCarDetail(String id) {
+		CarModel receivedCar = null;
+		for (CarModel car : archiveCar) {
+			if (car.getId().equalsIgnoreCase(id)) {
+				receivedCar = car;
+			}
+		}
+		return receivedCar;
+	}
+}
